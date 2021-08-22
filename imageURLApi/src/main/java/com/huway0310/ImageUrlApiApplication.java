65926af1c4fabc17ce6c11fd02b77c20ec80d94f
guowei <guowei@KuoweideMacBook-Pro.local>
2021-08-22 13:49:48 +0800

    FristCommit

---
+++ imageURLApi/src/main/java/com/huway0310/ImageUrlApiApplication.java
@@ -0,0 +1,13 @@
+package com.huway0310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageUrlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageUrlApiApplication.class, args);
+	}
+
+}
---
+++ imageURLApi/src/test/java/com/huway0310/ImageUrlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huway0310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageUrlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
